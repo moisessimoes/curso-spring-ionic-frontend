bf9f52d23bcd36502bcdb2fe1b956d8ed06ab42b
Moises <moisessimoes02@gmail.com>
2020-12-16 16:34:39 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,11 @@
 export const API_CONFIG = {
 
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+
+    //Nesse bucketBaseUrl seria a URL da conta da Amazon AWS com as imagens, mass eu não tenho conta lá.
+    //Essa URL é do professor do curso, é só para ilustrar aqui sobre como deveria ser, mas ná pratica não
+    //estou usando esse bucketBaseUrl.
+
+    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic/"
+
 }
